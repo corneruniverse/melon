f13e18fcbe876550f101b9208a59401c103467d9
Mambo V <19864849+mambov@users.noreply.github.com>
2024-10-18 16:05:30 -0500

    feat: add touch screen tap

--- melon/Scripts/crane.gd
+++ melon/Scripts/crane.gd
@@ -9,13 +9,25 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	get_input()
+	
 	pass
 	
-func get_input():
+func _unhandled_input(event):
+	# if the user taps the screen (on their smartphone)
+	# reference: https://docs.godotengine.org/en/stable/tutorials/inputs/inputevent.html
+	# reference: https://forum.godotengine.org/t/which-is-the-best-way-to-handle-touch-screen-controls-and-clicking-objects/42209
+	if event is InputEventKey:
+		if event.pressed:
+			get_input(event)
+	
+func get_input(event):
 	look_at(get_global_mouse_position())
-	if Input.is_action_just_pressed("ui_down"):
+	if Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("drop"):
 		drop()
+		
+	if (event is InputEventScreenTouch):
+		if (event.pressed()):
+			drop()
 
 func drop():
 	var m = melon_scene.instantiate()
