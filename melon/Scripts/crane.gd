6d44bcb9a1490f83ff157128ab81bcb14999ab1f
Mambo V <19864849+mambov@users.noreply.github.com>
2024-10-19 07:04:30 -0500

    fix: add StaticBody2D to Straw

--- melon/Scripts/crane.gd
+++ melon/Scripts/crane.gd
@@ -9,25 +9,29 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
+	get_input()
 	pass
 	
-func _unhandled_input(event):
-	# if the user taps the screen (on their smartphone)
-	# reference: https://docs.godotengine.org/en/stable/tutorials/inputs/inputevent.html
-	# reference: https://forum.godotengine.org/t/which-is-the-best-way-to-handle-touch-screen-controls-and-clicking-objects/42209
-	if event is InputEventKey:
-		if event.pressed:
-			get_input(event)
+#func _unhandled_input(event):
+	## if the user taps the screen (on their smartphone)
+	## reference: https://docs.godotengine.org/en/stable/tutorials/inputs/inputevent.html
+	## reference: https://forum.godotengine.org/t/which-is-the-best-way-to-handle-touch-screen-controls-and-clicking-objects/42209
+	#if event is InputEventKey:
+		#if event.pressed:
+			#get_input(event)
 	
-func get_input(event):
-	look_at(get_global_mouse_position())
+func get_input():
+	# rotate the crane
+	# removed in order to simplify and stabilize mobile app version
+	#look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("drop"):
 		drop()
 		
-	if (event is InputEventScreenTouch):
-		if (event.pressed()):
-			drop()
+	## if the touch screen was used	
+	#if (event is InputEventScreenTouch):
+		## if the touch screen was pressed
+		#if (event.pressed()):
+			#drop()
 
 func drop():
 	var m = melon_scene.instantiate()
