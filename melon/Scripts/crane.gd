8b888eb4618f19e93f5f580ff9358944c28ac619
Diego V <19864849+mambov@users.noreply.github.com>
2024-10-22 05:40:07 -0500

    feat: add click to move crane

--- melon/Scripts/crane.gd
+++ melon/Scripts/crane.gd
@@ -1,11 +1,26 @@
-extends Node2D
+extends CharacterBody2D
 #@export var melon_scene : PackedScene
 var melon_scene = preload("res://Scenes/melon.tscn")
+var speed = 400  # move speed in pixels/sec
+var target = null
+
+# the character moves to the clicked location.
+# reference: https://kidscancode.org/godot_recipes/4.x/2d/topdown_movement/index.html
+func _input(event):
+	if event.is_action_pressed("click"):
+		target = get_global_mouse_position()
+
+func _physics_process(delta):
+	if target:
+		# look_at(target)
+		velocity = position.direction_to(target) * speed
+		if position.distance_to(target) < 10:
+			velocity = Vector2.ZERO
+	move_and_slide()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -24,7 +39,8 @@ func get_input():
 	# rotate the crane
 	# removed in order to simplify and stabilize mobile app version
 	#look_at(get_global_mouse_position())
-	if Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("drop"):
+	#if Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("drop"):
+	if Input.is_action_just_pressed("drop"):
 		drop()
 		
 	## if the touch screen was used	
